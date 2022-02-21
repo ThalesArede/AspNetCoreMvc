ea05b83003ef9f1b068aec5c999adf1407ab9548
Thales Arêde <thales_arede@hotmail.com>
2022-02-20 21:30:13 -0300

    Add Migrations

--- SaleOfSnacks/Models/Categoria.cs
+++ SaleOfSnacks/Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace SaleOfSnacks.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SaleOfSnacks.Models
 {
+
+    [Table("Categorias")]
+
     public class Categoria // Entidade categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+        [StringLength(100,ErrorMessage ="O tamanho máximo é de 100 caracteres")]
+        [Required(ErrorMessage ="Informe o nome da categoria")]
+        [Display(Name ="Nome")]
         public string GategoriaNome { get; set; }
+        [StringLength(200, ErrorMessage = "O tamanho máximo é de 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Descrição")]
 
         public string Descrição { get; set; }
 
--- SaleOfSnacks/Models/Game.cs
+++ SaleOfSnacks/Models/Game.cs
@@ -1,20 +1,53 @@
-﻿namespace SaleOfSnacks.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SaleOfSnacks.Models
 {
+
+    [Table("Games")]
+
     public class Game // Entidade Game
     {
+        [Key]
+
         public int GameId { get; set; } // Lanche ID
+        
+        [Required(ErrorMessage = "O nome do game deve ser informado")]
+        [Display(Name = "Nome do Game")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} ")]       
         public string Nome { get; set; }
+       
+        [Required(ErrorMessage = "O nome do game deve ser informado")]
+        [Display(Name = "Nome do Game")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
         public string DescricaoCurta { get; set; }
+       
+        [Required(ErrorMessage = "O nome do game deve ser informado")]
+        [Display(Name = "Nome do Game")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
         public string DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do game")]
+        [Display(Name ="Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1,999.99,ErrorMessage ="O preço deve estar entre 1 e 999.99")]
         public decimal Preco { get; set; }
 
+        [Display(Name = "Caminho Imagem Normal")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemUrl { get; set; }
 
+        [Display(Name = "Caminho Imagem Normal")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemThumbnailUrl { get; set; }
 
-        public bool GameEfavorito { get; set; } // Is lanche preferido
-        public bool EmEstoque { get; set; }
+        [Display(Name = "Preferido")]
+        public bool IsGamePreferido { get; set; } // Is lanche preferido
 
+        [Display(Name = "Estoque")]
+        public bool EmEstoque { get; set; }
         public int CategoriaId { get; set; } 
         public virtual Categoria Categoria { get; set; } // Propriedades dos Games
 
